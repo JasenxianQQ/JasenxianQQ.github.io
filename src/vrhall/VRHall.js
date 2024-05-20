8b2f50882fb8ded2e486013583b4ccce92ef382b
Jasenxian <480323274@qq.com>
2024-05-20 23:06:00 +0800

    Describe your changes

--- src/vrhall/VRHall.js
+++ src/vrhall/VRHall.js
@@ -1,4 +1,4 @@
-import * as THREE from '@three/build/three.module.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 import CameraControls from '@controls/dist/camera-controls.module.js';
 import { GLTFLoader } from '@three/examples/jsm/loaders/GLTFLoader.js';
 import { TransformControls } from '@three/examples/jsm/controls/TransformControls.js';
